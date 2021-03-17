61eb16e5e0f64b080c6819ca6b0a473a77a99c6b
Benoit Bombonel <benoit.bombonel@hotmail.fr>
2021-03-17 16:16:47 +0100

    commit test

---
+++ app/components/Screen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Screen() {
+    return (
+        <View>
+            <Text>Coucou</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
