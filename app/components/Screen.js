18808d09c609f938ec0ef2f31e5745a7563c6679
Benoit Bombonel <benoit.bombonel@hotmail.fr>
2021-03-17 16:27:37 +0100

    envoie repo

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function Screen() {
     return (
         <View>
-            <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti libero odio ipsam animi minima voluptates provident laudantium nulla saepe voluptatibus hic, ducimus rem dolore, vel facilis excepturi doloribus quasi non possimus iste magni autem impedit delectus reiciendis! Quam, quo doloribus.</Text>
+            <Text>Ah que coucou bob</Text>
         </View>
     )
 }
