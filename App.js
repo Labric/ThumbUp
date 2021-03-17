f367131c85d77f8eaca1adbc1e613696f62cb6ae
Benoit Bombonel <benoit.bombonel@hotmail.fr>
2021-03-17 18:07:49 +0100

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Coucou!</Text>
+      <Text>Ah que coucou Bobo</Text>
       <StatusBar style="auto" />
     </View>
   );
