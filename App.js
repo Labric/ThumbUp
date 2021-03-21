8483645b9ec57173d545ea10837595cd1e9205ee
Benoit Bombonel <benoit.bombonel@hotmail.fr>
2021-03-21 11:53:19 +0100

    before eject

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './app/components/Main';
+import firebase from "./app/utils/FirebaseConfig"
+import StylesFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
 
 export default function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChange((user) => {
+      setIsSignedIn(!!user)
+    })
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Ah que coucou Bobo</Text>
-      <StatusBar style="auto" />
+      {isSignedIn ? (
+        <Main />
+      ) : (
+        <StylesFirebaseAuth 
+        uiConfig={uiConfig}
+        firebaseAuth={firebase.auth()}
+        />
+      )}
     </View>
   );
 }
---
+++ app/utils/FirebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+
+const FirebaseConfig = {
+  apiKey: "AIzaSyCgojIVo8hIPohZbzHmenu45PH6R-i2QpI",
+  authDomain: "thumbup-ee2fe.firebaseapp.com",
+  projectId: "thumbup-ee2fe",
+  storageBucket: "thumbup-ee2fe.appspot.com",
+  messagingSenderId: "1076691547183",
+  appId: "1:1076691547183:web:f0310c1215b779422282da"
+};
+  // Initialize Firebase
+  firebase.initializeApp(FirebaseConfig);
+
+export default firebase
